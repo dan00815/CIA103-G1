763d25a9562345fb6951f3b35e1508d5516262c9
WenFang <rfang1221@gmail.com>
2024-12-15 16:50:40 +0800

    新增訂單匯出+新增字體+修正綠界+更改mail時間顯示

--- src/main/java/com/event/cia103g1springboot/product/product/controller/CartController.java
+++ src/main/java/com/event/cia103g1springboot/product/product/controller/CartController.java
@@ -1,6 +1,8 @@
 package com.event.cia103g1springboot.product.product.controller;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -322,47 +324,50 @@ public class CartController {
 		return "front-end/shop/myPdtOrder";
 	}
 
-//	@GetMapping("/get_myPdtOrderItem")
-//	public String get_myPdtOrderItem(
-//			@RequestParam("pdtOrderId") String pdtOrderId, 
-//			@RequestParam("orderStat") String orderStat, 
-//			@RequestParam("orderDate") String orderDate, 
-//			Model model, HttpSession session) {
-//		
-//	    // 日期解析和格式化器
-//	    DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-//	    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//	    // 解析和格式化
-//	    String formattedDate = LocalDateTime.parse(orderDate, inputFormatter).format(outputFormatter);
-//	    // 傳遞格式化後的日期
-//	    model.addAttribute("orderDate", formattedDate);
-//		
-//		// 顯式轉換 orderStat
-//	    Integer pdtOrderId2= Integer.valueOf(pdtOrderId);
-//	    model.addAttribute("pdtOrderId", pdtOrderId2);
-//	    model.addAttribute("orderStat", Integer.valueOf(orderStat));
-//	    
-//	    
-//	    List<ProductOrderItemVO> list = pdtOrderItemSvc.getOrderItemByPdtOrderId(pdtOrderId2);
-//	    //計算總金額
-//	    Integer totalAmount = 0; // 定義總金額變數
-//	    
-//	    //計算每個項目的小計並累加總金額
-//	    for (ProductOrderItemVO item : list) {
-//	    	Integer subtotal = item.getPdtPrice() * item.getOrderQty();
-//	        totalAmount += subtotal;    // 累加小計
-//	    }
-//
-//	    // 將結果加入模型
-//	    model.addAttribute("pdtOrderItemListData", list); // 傳遞訂單項目列表
-//	    model.addAttribute("totalAmount", totalAmount); // 傳遞總金額
-//	    
-//	    
-//		// 返回訂單明細頁面
-//		return "front-end/shop/myPdtOrderItem";
-//	}
+	@GetMapping("/get_myPdtOrderItem")
+	public String get_myPdtOrderItem(
+			@RequestParam("pdtOrderId") String pdtOrderId, 
+			@RequestParam("orderStat") String orderStat, 
+			@RequestParam("orderDate") String orderDate, 
+			Model model) {
+		
+	    // 日期解析和格式化器
+	    DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+	    DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	    // 解析和格式化
+	    String formattedDate = LocalDateTime.parse(orderDate, inputFormatter).format(outputFormatter);
+	    // 傳遞格式化後的日期
+	    model.addAttribute("orderDate", formattedDate);
+		
+		// 顯式轉換 orderStat
+	    Integer pdtOrderId2= Integer.valueOf(pdtOrderId);
+	    model.addAttribute("pdtOrderId", pdtOrderId2);
+	    model.addAttribute("orderStat", Integer.valueOf(orderStat));
+	    
+	    //新修正未確認過
+	    List<ProductOrderItemVO> list = pdtOrderItemSvc.getOrderItemsByPdtOrderId(pdtOrderId2);
+	    //計算總金額
+	    Integer totalAmount = 0; // 定義總金額變數
+	    
+	    //計算每個項目的小計並累加總金額
+	    for (ProductOrderItemVO item : list) {
+	    	Integer subtotal = item.getPdtPrice() * item.getOrderQty();
+	        totalAmount += subtotal;    // 累加小計
+	    }
 
-	// 綠界備用
+	    // 將結果加入模型
+	    model.addAttribute("pdtOrderItemListData", list); // 傳遞訂單項目列表
+	    model.addAttribute("totalAmount", totalAmount); // 傳遞總金額
+	    
+	    
+		// 返回訂單明細頁面
+		return "front-end/shop/myPdtOrderItem";
+	}
+
+	
+	
+	
+	// 綠界備用不用開
 //	@PostMapping("/ecpayCheckout")
 //	public String ecpayCheckout(Model model, HttpSession session) {
 //		String uuId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
