c42f43a05ccb4fbaf2c7628a421fb087d051c825
Rui-Xian-45 <zxc0930188136@gmail.com>
2024-12-16 16:04:43 +0800

    修改了 會員-行程明細 畫面

--- src/main/java/com/event/cia103g1springboot/plan/planorder/model/PlanOrderRepository.java
+++ src/main/java/com/event/cia103g1springboot/plan/planorder/model/PlanOrderRepository.java
@@ -2,8 +2,12 @@ package com.event.cia103g1springboot.plan.planorder.model;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +20,20 @@ public interface PlanOrderRepository extends JpaRepository<PlanOrder, Integer> {
     List<PlanOrder> findAll(Sort sort);
 
     List<PlanOrder> findByMemVO_MemId(Integer memId);
+    @Query("SELECT p FROM PlanOrder p " +
+            "WHERE p.memVO.memId = :memId " +
+            "AND (p.plan.startDate <= :now AND p.plan.endDate >= :now OR p.plan.endDate >= :now)")
+    List<PlanOrder> findActiveOrdersByMember(@Param("memId") Integer memberId, @Param("now") LocalDate now);
 
-  PlanOrder findByPlanOrderIdAndOrderStat(Integer id, Integer status);
+
+
+
+
+
+    List<PlanOrder> findByMemVO_MemIdAndPlan_EndDateBefore(Integer memId, LocalDate now);
+
+
+    PlanOrder findByPlanOrderIdAndOrderStat(Integer id, Integer status);
 
     PlanOrder findByMemVO_MemIdAndPlanOrderId(Integer memId, Integer planOrderId);
 }
