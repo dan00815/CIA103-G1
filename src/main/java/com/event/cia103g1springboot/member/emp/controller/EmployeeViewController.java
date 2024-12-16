d99b2be3783824ee424bc05650699efcd877ed86
ahxuan <xuan45678901@gmail.com>
2024-12-16 17:10:25 +0800

    員工管理新增頁面

--- src/main/java/com/event/cia103g1springboot/member/emp/controller/EmployeeViewController.java
+++ src/main/java/com/event/cia103g1springboot/member/emp/controller/EmployeeViewController.java
@@ -1,4 +1,5 @@
 package com.event.cia103g1springboot.member.emp.controller;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,43 +15,47 @@ import com.event.cia103g1springboot.member.emp.model.EmployeeVO;
 @RequestMapping("/emp")
 public class EmployeeViewController {
 
-	@Autowired
-	private EmployeeService employeeService;
+    @Autowired
+    private EmployeeService employeeService;
 
-	@GetMapping("/login")
-	public String login() {
-		return "backindexlogin"; // 正確的視圖名稱
-	}
+    @GetMapping("/login")
+    public String login() {
+        return "emp/login";
+    }
 
-	@GetMapping("/edit/{id}")
-	public String editEmployee(@PathVariable("id") Integer id, Model model) {
-		EmployeeVO employee = employeeService.getEmployeeProfile(id);
-		model.addAttribute("employee", employee);
-		return "back-end/emp/edit";
-	}
+    @GetMapping("/show")
+    public String showEmployeeInfo(HttpSession session, Model model) {
+        EmployeeVO loginUser = (EmployeeVO) session.getAttribute("loginUser");
+        if (loginUser != null) {
+            model.addAttribute("employee", loginUser);
+            return "back-end/emp/show";
+        }
+        return "redirect:/emp/login";
+    }
 
-	// 添加其他視圖名稱的更新
-	@GetMapping("/list")
-	public String listEmployees(Model model) {
-		// 添加你的業務邏輯
-		return "back-end/emp/list"; // 更新視圖名稱
-	}
-
-	@GetMapping("/register")
-
-	public String registerEmployee() {
-
-		return "back-end/emp/register"; // 更新視圖名稱
-	}
-
-	@GetMapping("/reset-password")
-
-	public String resetPassword() {
-		return "back-end/emp/reset_password"; // 更新視圖名稱
-	}
-
+    @GetMapping("/edit/{id}")
+    public String editEmployee(@PathVariable("id") Integer id, Model model) {
+        EmployeeVO employee = employeeService.getEmployeeProfile(id);
+        model.addAttribute("employee", employee);
+        return "emp/edit";
+    }
 }
 
+
+
+
+//package com.example.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import com.example.model.EmployeeService;
+//import com.example.model.EmployeeVO;
+//
 //@Controller
 //@RequestMapping("/emp")
 //public class EmployeeViewController {
@@ -60,7 +65,7 @@ public class EmployeeViewController {
 //
 //    @GetMapping("/login")
 //    public String login() {
-//        return "/backindexlogin";
+//        return "emp/login";
 //    }
 //
 //    @GetMapping("/edit/{id}")
